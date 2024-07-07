84923f387dc3c00728c7e8be86d3410b672c8b22
Gergo Kaszas <gergo.kaszas@outlook.com>
2024-07-08 09:44:04 +1000

    Initial commit

---
+++ ManimGUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ManimGUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
