7322e8be1861c6eb8e535de0fadbb79a8af64e1f
Gergo Kaszas <gergo.kaszas@outlook.com>
2024-07-16 12:01:01 +1000

    EditorMenuManager

---
+++ ManimGUI/Windows/EditorWindow/EditorMenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace ManimGUI.Windows.EditorWindow
+{
+    public class EditorMenuManager : INotifyPropertyChanged
+    {
+        public ICommand ExitCommand { get; private set; }
+
+        public EditorMenuManager()
+        {
+            ExitCommand = new Command(Exit);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void Exit()
+        {
+            Application.Current.Quit();
+        }
+    }
+}
--- ManimGUI/Windows/EditorWindow/EditorPage.xaml.cs
+++ ManimGUI/Windows/EditorWindow/EditorPage.xaml.cs
@@ -1,22 +1,25 @@
-﻿namespace ManimGUI.Windows.EditorWindow;
+﻿using System.Windows.Input;
+
+namespace ManimGUI.Windows.EditorWindow;
 
 public partial class EditorPage : ContentPage
 {
 	public EditorPage()
 	{
 		InitializeComponent();
-	}
+        BindingContext = new EditorMenuManager();
+    }
     protected override void OnHandlerChanged()
     {
         base.OnHandlerChanged();
-#if WINDOWS
-                  var window = App.Current.Windows.FirstOrDefault().Handler.PlatformView as Microsoft.UI.Xaml.Window;
-                  IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
-                  Microsoft.UI.WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
-                  Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
-                (appWindow.Presenter as Microsoft.UI.Windowing.OverlappedPresenter).Maximize();
-            // this line can maximize the window
-#endif
-    }
+    #if WINDOWS
+                      var window = App.Current.Windows.FirstOrDefault().Handler.PlatformView as Microsoft.UI.Xaml.Window;
+                      IntPtr windowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                      Microsoft.UI.WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowHandle);
+                      Microsoft.UI.Windowing.AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+                    (appWindow.Presenter as Microsoft.UI.Windowing.OverlappedPresenter).Maximize();
+                // this line can maximize the window
+    #endif
 
+    }
 }
