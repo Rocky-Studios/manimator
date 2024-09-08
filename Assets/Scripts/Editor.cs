4540c9e606bea478f97776d7fdecca322738cea5
Gergo Kaszas <gergo.kaszas@outlook.com>
2024-09-08 19:27:51 +1000

    Rename

--- Assets/Scripts/Editor.cs
+++ Assets/Scripts/Editor.cs
@@ -98,7 +98,6 @@ public partial class Editor : Control
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		GD.Print(CurrentFrame);
 		// If frame counter isn't being edited then set the frame to the current frame
 		if(!GetNode<TextEdit>("Background/Screen/Row1/Preview/Frame").HasFocus()) GetNode<TextEdit>("Background/Screen/Row1/Preview/Frame").Text = CurrentFrame.ToString();
 
